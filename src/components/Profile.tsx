8c074d8030b8cec86238893694b02bae1c5b1270
Luís Clício <cliciosa14@gmail.com>
2021-03-21 14:55:05 -0300

    feat: fourth class

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+
+import { ChallengeContext } from '../contexts/ChallengeContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengeContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/darktechlc.png" alt="Luís Clício" />
@@ -8,7 +14,7 @@ export function Profile() {
         <strong>Luís Clício</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
